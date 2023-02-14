f565a430c51e2b726c5646233ec12530593f5aac
皮邱 <yong_qiu_ch@163.com>
2023-02-14 19:59:28 +0800

    init 2

---
+++ bootstrap/bootstrap.go
@@ -0,0 +1,10 @@
+package bootstrap
+
+import (
+	"fmt"
+)
+
+func Hello() error{
+	fmt.Print("hello start it!!")
+	return nil
+}
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/piqiu96/netproxy/util"
+	"github.com/piqiu96/netproxy/bootstrap"
 )
 
 func main() {
 	a := util.GotStr()
 	fmt.Println(a)
 	fmt.Println("vim-go")
+
+	bootstrap.Hello()
 }
